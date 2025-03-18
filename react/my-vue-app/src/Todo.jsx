ce0cfd5e1e534f33e94087323f47b70ac24537d1
Sinthiya Akter Mim <info.sinthiyaaktermim@gmail.com>
2025-03-18 10:16:13 +0600

    conditional rendering

--- react/my-vue-app/src/Todo.jsx
+++ react/my-vue-app/src/Todo.jsx
@@ -17,15 +17,33 @@
     
   
 // }
-export default function Todo({task,isDone}){
-  if(isDone){
-    return <li>finished:{task}</li>
-  }
-    return <li>
-      worked one:{task}
-    </li>
-  }
+// export default function Todo({task,isDone}){
+//   if(isDone){
+//     return <li>finished:{task}</li>
+//   }
+//     return <li>
+//       worked one:{task}
+//     </li>
+//   }
+      // conditonal rendering 3
+
+      // export default function Todo({task,isDone}){
+        
+      //     return(
+      //     <li>{isDone ?'finished':'worked on'};{task}</li>
+      //     ) 
+         
+      //   }
+
+      export default function Todo({task,isDone}){
       
+          return(
+             <li>{task} {isDone || ':do it'}</li>
+          )
+        }
+         
+        
+          
     
   
 
